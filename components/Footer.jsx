d05e67e9fe26a61a38be171788813981e88751cd
M Dhanush <mdhanush11923@gmail.com>
2024-10-05 02:53:19 +0530

    update button

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "@nextui-org/react";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col home-footer w-full h-40 mt-16 gap-2 p-10 sm:h-60 bg-myhouseblue opacity-85 justify-center text-center items-center">
+    <footer className="flex flex-col home-footer w-full h-40 mt-10 gap-2 p-10 sm:h-60 bg-myhouseblue opacity-85 justify-center text-center items-center">
       <p className="text-white">
         &copy; 2024 PIZzA Delivery. All rights reserved.
       </p>
